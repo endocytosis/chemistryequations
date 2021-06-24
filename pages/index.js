636f7c9d6da4e8e998b8a1e2eb5ac42e8002a8e8
endocytosis <brian_t.gallagher@tufts.edu>
2021-06-23 22:38:48 -0400

    Chemistry equations

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Chemistry Equations</title>
       </Head>
 
       <main className={styles.main}>
