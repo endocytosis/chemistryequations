fd1362891dbe1cb35d70726ea7298e14fda15365
endocytosis <brian_t.gallagher@tufts.edu>
2021-10-18 18:07:29 -0400

    1 file modified

--- pages/net.js
+++ pages/net.js
@@ -3,8 +3,10 @@ import styles from '../styles/pages.module.css';
 function Net() {
   return (
     <div classname={styles.container}>
+      <title>Net Ionic Equations</title>
       <main className={styles.main}>
         <p>Net Ionic Equations are a topic of frequent study.</p>
+        <a href="#">Test</a>
       </main>
     </div>
   );
