955d393a376825a9182b22496990dd9b9cc0435f
endocytosis <brian_t.gallagher@tufts.edu>
2021-10-18 17:21:56 -0400

    1 file modified

--- pages/net.js
+++ pages/net.js
@@ -3,28 +3,8 @@ import Head from 'next/head';
 export default function Net() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Net Ionic Equations</h1>
-      </main>
       <title>Net Ionic Equations</title>
-      <body>
-        <p>Net ionic equations are a topic of frequent study.</p>
-      </body>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://stackblitz.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by StackBlitz{' '}
-          <img
-            src="https://c.staticblitz.com/assets/favicon-7453cf0c12d349fb64b7aa2b69cc69c026f083a27f139f0839b1f4948bed6811.png"
-            width="20"
-            height="20"
-          />
-        </a>
-      </footer>
+      <p>Net Ionic Equations are a topic of frequency study.</p>
     </div>
   );
 }
