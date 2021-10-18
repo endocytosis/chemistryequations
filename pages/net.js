48faeb1faed30d771e4d1bda06a37c2984abf32b
endocytosis <brian_t.gallagher@tufts.edu>
2021-10-18 17:34:14 -0400

    1 file modified

--- pages/net.js
+++ pages/net.js
@@ -2,7 +2,10 @@ export default function Net() {
   return (
     <div className={styles.body}>
       <title>Net Ionic Equations</title>
-      <p>Net Ionic Equations are a topic of frequency study.</p>
+      <p>
+        A net ionic equation is the full ionic equation absent the spectator
+        ions.
+      </p>
     </div>
   );
 }
