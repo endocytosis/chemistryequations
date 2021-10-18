89b136af7e78e1b1e22f3446771c2a726c1a55ac
endocytosis <brian_t.gallagher@tufts.edu>
2021-10-18 18:37:52 -0400

    3 files modified

--- pages/net.js
+++ pages/net.js
@@ -9,6 +9,10 @@ function Net() {
           Net ionic equations are complete ionic equations absent the spectator
           ions. Let's look at the following reaction between sodium chloride and
           silver nitrate:
+          <MathJaxContext>
+            <h2>Basic MathJax example with Latex</h2>
+            <MathJax>{'\\(\\frac{10}{4x} \\approx 2^{12}\\)'}</MathJax>
+          </MathJaxContext>
         </p>
       </main>
     </div>
